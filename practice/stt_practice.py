0418f4e8a027fa70aad4c1ea98c7a58cfac357bd
JaehyoJJAng <yshrim12@naver.com>
2023-01-29 15:04:55 +0900

    fet: set_audio_file 함수 추가 - 파일로부터 음성 불러오기

--- practice/stt_practice.py
+++ practice/stt_practice.py
@@ -1,19 +1,43 @@
 import speech_recognition as sr
 
-# Create Recognizer Instance
-r : sr.Recognizer = sr.Recognizer()
+def set_speech()-> None:
+    # Create Recognizer Instance    
+    r : sr.Recognizer = sr.Recognizer()
 
-# 마이크를 통해서 들려오는 음성 저장
-with sr.Microphone() as source:
-    print('듣고 있어요')
-    audio = r.listen(source=source)
+    # Set MicroPhone
+    # audio : sr.AudioData = set_micro_phone(r=r)
+    
+    # Set Audio File
+    audio : sr.AudioFile = set_audio_file(r=r)
+    
+    try:
+        # 구글 API 로 인식 (하루 50회)
+        # 영어 문장
+        # text = r.recognize_google(audio_data=audio,language='en-US')
+        
+        # 한글 문장
+        text = r.recognize_google(audio_data=audio,language='ko')
+        print(text)
+    except sr.UnknownValueError:
+        print('인식 실패')   # 음성 인식 실패한 경우
+    except sr.RequestError as rq_err: # # API Key 오류 또는 네트워크 단절 등 ..   
+        print(f'요청 실패 : {rq_err}')
+    
+def set_micro_phone(r:sr.Recognizer)-> sr.AudioData:
+    """ 마이크로부터 음성 듣기"""
+    with sr.Microphone() as source:
+        print('듣고 있어요')
+        audio : sr.AudioData = r.listen(source=source)
+        return audio
 
-try:
-    # 구글 API 로 인식 (하루 50회)
-    # 영어 문장
-    text = r.recognize_google(audio_data=audio,language='en-US')
-    print(text)
-except sr.UnknownValueError:
-    print('인식 실패')   # 음성 인식 실패한 경우
-except sr.RequestError as rq_err: # # API Key 오류 또는 네트워크 단절 등 ..   
-    print(f'요청 실패 : {rq_err}')
+def set_audio_file(r:sr.Recognizer)-> sr.AudioFile:
+    """ 파일로부터 음성 불러오기 (.wav , .aiff , .flac) """
+    with sr.AudioFile('sample.wav') as source:
+        audio : sr.AudioFile  = r.record(source=source)
+        return audio 
+
+def main():
+    set_speech()
+
+if __name__ == '__main__':
+    main()
