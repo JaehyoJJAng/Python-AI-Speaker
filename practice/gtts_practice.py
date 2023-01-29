370fa5c5e7c70c6c14c3fdcf7c55bdaabf5665eb
JaehyoJJAng <yshrim12@naver.com>
2023-01-29 14:36:13 +0900

    fet: set_text() - 파일에서 텍스트 불러와서 처리

--- practice/gtts_practice.py
+++ practice/gtts_practice.py
@@ -3,12 +3,10 @@
 from gtts import gTTS
 from playsound import playsound
 
-
-def set_text()-> str:
-    # 영어문장
-    # return 'Can I Help You?'
-    # 한글문장
-    return '파이썬을 배우면 이런 것도 할 수 있어요'
+def set_text(source_txt:str)-> str:
+    # 파일에서 불러와서 처리    
+    with open(source_txt,'r',encoding='UTF-8') as fp:
+        return fp.read()
 
 def set_file()-> str:
     return 'sample.mp3'
@@ -17,13 +15,16 @@ def set_gTTS(text:str,file_name:str)-> None:
     tts_en : gTTS = gTTS(text=text,lang='ko')
     tts_en.save(savefile=file_name) 
 
-def run_mp3(file_name:str):
+def run_mp3(file_name:str): 
     playsound(file_name)
     
 def main()-> None:
-    # Text
-    text : str = set_text()
+    # Source Text File
+    source_txt : str = 'sample.txt'
     
+    # Text
+    text : str = set_text(source_txt=source_txt)
+        
     # file name
     file_name : str = set_file()
     
