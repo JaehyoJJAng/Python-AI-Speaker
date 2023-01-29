d99d08a53afdebde2b9debd71374040ce2086ea4
JaehyoJJAng <yshrim12@naver.com>
2023-01-29 14:29:40 +0900

    fet: run_mp3 추가 - playsound 라이브러리 사용

--- practice/gtts_practice.py
+++ practice/gtts_practice.py
@@ -1,6 +1,7 @@
 # TTS (Text To Speech)
 # STT (Speech To Text)
 from gtts import gTTS
+from playsound import playsound
 
 
 def set_text()-> str:
@@ -13,6 +14,9 @@ def set_gTTS(text:str,file_name:str)-> None:
     tts_en : gTTS = gTTS(text=text,lang='en')
     tts_en.save(savefile=file_name) 
 
+def run_mp3(file_name:str):
+    playsound(file_name)
+    
 def main()-> None:
     # Text
     text : str = set_text()
@@ -22,6 +26,9 @@ def main()-> None:
     
     # gTTS
     set_gTTS(text=text,file_name=file_name)
+    
+    # run .mp3
+    run_mp3(file_name=file_name)
 
 if __name__ == '__main__':
     main()
