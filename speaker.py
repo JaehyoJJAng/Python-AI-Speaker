9ce71eb9cd27410d7b431a4fdc4a3742bffe690b
JaehyoJJAng <yshrim12@naver.com>
2023-01-29 16:23:10 +0900

    fet: answer 메소드 구현

--- speaker.py
+++ speaker.py
@@ -13,14 +13,11 @@ class AISpeaker:
         m : sr.Microphone() = sr.Microphone()
         
         # 백그라운드로 계속 열어둠
-        stop_listening : r = r.listen_in_background(m,self.listen)
+        self.stop_listening : r = r.listen_in_background(m,self.listen)
         
         # 안내메시지
         self.speak(text='무엇을 도와드릴까요?')
-                
-        # 더 이상 듣지 않음
-        # stop_listening(wait_for_stop=False)
-        
+                        
         # 프로그램 무한 반복
         while True:
             time.sleep(0.1)            
@@ -30,15 +27,35 @@ class AISpeaker:
         try:
             text = recognizer.recognize_google(audio,language='ko')
             print(f'[User] : {text}')
+            
+            # answer 메소드 호출
+            self.answer(input_text=text)
+
         except sr.UnknownValueError:
             print('인식 실패')   # 음성 인식 실패한 경우
+
         except sr.RequestError as rq_err: # # API Key 오류 또는 네트워크 단절 등 ..   
             print(f'요청 실패 : {rq_err}')
     
     def answer(self,input_text:str)-> None: 
         """ 대답 """
-        pass
-    
+        answer_text : str = '' # 인공지능 대답        
+        if '안녕' in input_text:
+            answer_text = '안녕하세요 저는 인공지능입니다.'
+        elif '날씨' in input_text:
+            answer_text = '오늘의 서울 기온은 추울 것 같아요!'
+        elif '고마워' in input_text:
+            answer_text = '별 말씀을요!'
+        elif '종료' in input_text:
+            # 더 이상 듣지 않음
+            answer_text = '다음에 또 만나요'
+            self.stop_listening(wait_for_stop=False)
+        else:
+            answer_text = '잘 모르겠어요 ..'
+
+        # 인공지능 speak
+        self.speak(text=answer_text)
+        
     def speak(self,text:str)-> None:
         # 인공지능 출력문
         print(f'[인공지능] : {text}')
@@ -57,8 +74,8 @@ class AISpeaker:
             
     def play_sound(self,file_name:str):
         """ MP3 Play """
-        playsound(file_name)
-        
+        playsound(file_name)    
+    
 def main()-> None:
     # Create AISpeaker Instance
     ai_speaker : AISpeaker = AISpeaker()
